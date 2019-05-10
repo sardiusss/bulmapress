9d8aac0b79c433d80a4581c3c50aeb025a2ae657
Jonathan <jonathantwyman@gmail.com>
2019-05-10 23:23:04 +0100

    Update navigation.php
    Function not called due to it being found. in-correct negated

--- functions/navigation.php
+++ functions/navigation.php
@@ -6,7 +6,7 @@
  */
 
 // This theme uses wp_nav_menu() in one location.
-if ( ! function_exists( 'register_nav_menus' ) ) {
+if ( function_exists( 'register_nav_menus' ) ) {
 	register_nav_menus( array(
 		'menu-1' => esc_html__( 'Primary', 'bulmapress' ),
 		) );
